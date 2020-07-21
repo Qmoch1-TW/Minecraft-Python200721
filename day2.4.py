211a898e5e5fde033000d032e46604597d7458c2
Qmoch1-TW <68536853+Qmoch1-TW@users.noreply.github.com>
2020-07-21 16:45:58 +0800

    Add files via upload

---
+++ day2.4.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 11:20:18 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+import time
+import random
+
+ 
+
+
+while True:
+    color=random.randrange(0,9)
+    x,y,z=mc.player.getTilePos()
+    mc.setBlock(x,y,z-1,38,color)
+    time.sleep(0.2)
+    
+    
---
+++ day2.6.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 14:40:13 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+
+x,y,z=mc.player.getTilePos()
+
+blockid = int(input("輸入方塊id:"))
+mc.setBlock(x+1,y,z,blockid)
+
+
